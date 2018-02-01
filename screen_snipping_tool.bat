df1c679dad7a9e1ca1f1ce550414adf6588c4e6e
yuqiang <932967248@qq.com>
2018-02-01 15:55:13 +0800

    Screen Snipping Tool V1.1.1 Fixed a bug in "screen_snipping_tool.bat".

--- screen_snipping_tool.bat
+++ screen_snipping_tool.bat
@@ -1,2 +1,2 @@
 CD %~dp0
-D:\ProgramFiles\Python\Anaconda-x64\pythonw.exe screen_snipping_tool.py -o ScreenSnippingImages/ScreenSnippingImage_%Y-%m-%d_%H-%M-%S.png -l screen_snipping_tool.log -s 1000
+D:\ProgramFiles\Python\Anaconda-x64\pythonw.exe screen_snipping_tool.py -o "ScreenSnippingImages/ScreenSnippingImage_%%Y-%%m-%%d_%%H-%%M-%%S.png" -l screen_snipping_tool.log -s 1000
